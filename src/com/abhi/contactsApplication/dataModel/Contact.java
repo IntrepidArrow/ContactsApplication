6b09445c77dd46e23d68ec4e041bd2f1d8e49f86
Abhimukth Chaudhuri <abhimukth.chaudhuri@mail.mcgill.ca>
2019-05-28 14:57:36 -0400

    Add Contacts class with required generic methods

--- src/com/abhi/contactsApplication/dataModel/Contact.java
+++ src/com/abhi/contactsApplication/dataModel/Contact.java
@@ -1,4 +1,76 @@
 package com.abhi.contactsApplication.dataModel;
 
+import javafx.beans.property.SimpleStringProperty;
+
 public class Contact {
+    //class fields
+    private SimpleStringProperty firstName;
+    private SimpleStringProperty lastName;
+    private SimpleStringProperty phoneNumber;
+    private SimpleStringProperty notes;
+
+    public Contact(String firstName, String lastName, String phoneNumber, String notes) {
+        this.firstName.set(firstName);
+        this.firstName.set(lastName);
+        this.firstName.set(phoneNumber);
+        this.firstName.set(notes);
+    }
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public SimpleStringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public SimpleStringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber.get();
+    }
+
+    public SimpleStringProperty phoneNumberProperty() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber.set(phoneNumber);
+    }
+
+    public String getNotes() {
+        return notes.get();
+    }
+
+    public SimpleStringProperty notesProperty() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes.set(notes);
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", phoneNumber=" + phoneNumber +
+                ", notes=" + notes +
+                '}';
+    }
 }
